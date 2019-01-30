489190a4aff35bbf4729b1a3593aacc3c00a6a01
ColtonMumley <colton12@vt.edu>
2019-01-30 13:44:20 -0500

    updated cards

--- src/components/Teams/Software/Repos.jsx
+++ src/components/Teams/Software/Repos.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { Grid, Col, Row } from "react-bootstrap";
+import { Card } from "antd";
+
+import "../../../assets/css/Repos.css";
 const Octokit = require("@octokit/rest");
 const octokit = new Octokit();
 
@@ -14,20 +18,55 @@ class Repos extends Component {
     this.getRepos();
   }
 
+  // Gets a list of most recently updated repos
   getRepos() {
     octokit.repos
-      .listForUser({ username: "RocketryVT" })
+      .listForUser({
+        username: "RocketryVT",
+        sort: "updated",
+        per_page: 5,
+        page: 1
+      })
       .then(resp => this.setState({ repos: resp.data }));
   }
 
+  // Gets profile image url of top contributor to repo
+  getTopContributor() {
+    let me = octokit.repos
+      .listContributors({
+        owner: "RocketryVT",
+        repo: "Website"
+      })
+      .then(resp => resp.data[0].avatar_url);
+    console.log(me);
+  }
+
+  // Returns cards of repos
   showRepos() {
     return this.state.repos.map((repo, index) => {
-      return <p key={index}>{repo.name}</p>;
+      return (
+        <Card
+          key={index}
+          className="repo-cards"
+          cover={<img alt="card cover" src="" />}
+        >
+          <p>{repo.name}</p>
+        </Card>
+      );
     });
   }
 
   render() {
-    return <div>{this.showRepos()}</div>;
+    return (
+      <Grid fluid className="card-container">
+        {this.getTopContributor()}
+        <Row>
+          <Col sm={6} md={12}>
+            {this.showRepos()}
+          </Col>
+        </Row>
+      </Grid>
+    );
   }
 }
 
