1948d19586118a186bad730b2b6b7590f0734dc8
ColtonMumley <colton12@vt.edu>
2019-01-30 12:12:06 -0500

    repos output in a list

--- src/components/Teams/Software/Repos.jsx
+++ src/components/Teams/Software/Repos.jsx
@@ -3,14 +3,31 @@ const Octokit = require("@octokit/rest");
 const octokit = new Octokit();
 
 class Repos extends Component {
+  constructor() {
+    super();
+    this.state = {
+      repos: []
+    };
+  }
+
+  componentDidMount() {
+    this.getRepos();
+  }
+
   getRepos() {
     octokit.repos
       .listForUser({ username: "RocketryVT" })
-      .then(resp => console.log(resp));
+      .then(resp => this.setState({ repos: resp.data }));
+  }
+
+  showRepos() {
+    return this.state.repos.map((repo, index) => {
+      return <p key={index}>{repo.name}</p>;
+    });
   }
 
   render() {
-    return <div>{this.getRepos()}</div>;
+    return <div>{this.showRepos()}</div>;
   }
 }
 
