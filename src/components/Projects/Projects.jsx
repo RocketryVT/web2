98b251207f653a7861efafa07868c0205c0ed7ec
ColtonMumley <colton12@vt.edu>
2019-03-26 17:49:01 -0400

    added sample text for rocket stats

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,27 @@ class Projects extends Component {
   render() {
     return (
       <div className="container-fluid bg">
-        <Col md={6} />
+        <Col md={6}>
+          <div className="info">
+            <h1>Here are some stats about the rocket:</h1>
+            <h4>
+              This rocket is really really fast. It can go 0-60 in at least 15
+              second. It can go basically the same speed as the ISS. In fact, I
+              think once we launch it, it will reach escape velocity and get
+              lost in space. To counteract this, we are attaching trashbags to
+              the fins as a sort of makeshift drogue chute. If we attach the
+              right number of trashbags, our rocket will reach the intended
+              altitude of 10,000 ft. At that point, which we all know is called
+              apogee, the payload will deploy. Our payload this year is just a
+              bunch of fireworks. However, they aren't just any old fireworks.
+              They will display a dazzling portrait of our most valuable team
+              member, Colton Mumley. On descent, the rocket will initiate the
+              landing boosters, similar to the the Falcon 9, except better. Once
+              it lands flawlessly, confetti will shoot out of the top and the
+              onboard speakers will play Shooting Stars by Bag Raiders.
+            </h4>
+          </div>
+        </Col>
         <Col md={6}>
           <div className="rocket">
             <img src={rocket} alt="rocket" />
